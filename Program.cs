a35de0515882aa50cbedd3e6849a68b3fcbd9617
Letícia <leticiazattera949@gmail.com>
2024-11-08 23:20:18 -0300

    criação do projeto

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+//criar um novo projeto console, dotnet new console
+//rodar o projeto dotnet run
+Console.WriteLine("Hello, World!");
